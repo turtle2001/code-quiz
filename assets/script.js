689b23b0399bae3c04b902916d0353f004319c91
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-03 23:46:27 -0700

    improvements?

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,49 @@
-var question0 = {
-    question: q1,
-    answers: ["q", "w", "e", "r"],
-    answer: answers[0]
-}
+var questionBox = document.getElementById("question-box");
+var startButton = document.getElementById("start-button");
+var timer = document.getElementById("timer");
 
-var questions = [question0];
+//question list
+var questionList = [{
+    question: "q1",
+    answers: ["q", "w", "e", "r"],
+    answer: "q"
+}, {
+    question: "q2",
+    answers: ["a", "s", "d", "f"],
+    answer: "f"
+}];
+
 var highscores;
 var numScores;
-//var currentQ;
 
-startButton.addEventListener("clcik", function () {
-    var cuurentQ = 0;
-    var correct = 0;
-
-    quizStart();
-})
+startButton.addEventListener("click", quizStart);
 
 function quizStart() {
-    if (currentQuestion < questions.length) {
-        var question = questions[currentQuestion];
+    var currentQuestion = 0;
+    //prints question
+    if (currentQuestion < questionList.length) {
         var qHeader = document.createElement("h2");
         qHeader.setAttribute("id", "qHeader");
-        qHeader.textContent(questions[cuurentQ]);
+        qHeader.textContent = questionList[currentQuestion].question;
+        questionBox.append(qHeader);
 
-        for (i = 0; i < questions.answer.length; i++) {
-            var answer = questions;
+        //lists answer
+        for (i = 0; i < questionList[currentQuestion].answers.length; i++) {
             var aHeader = document.createElement("h2");
-            aHeader.setAttribute("id", "aHeader")
-            aHeader.textContent(questions.answer[i]);
+            aHeader.setAttribute("id", "aHeader");
+            aHeader.textContent = questionList[currentQuestion].answers[i];
+            questionBox.append(aHeader);
+        }
 
-            aHeader.addEventListener("click", checkCorrect(textContent))
+        aHeader.addEventListener("click", checkCorrect(aHeader.textContent));
+
+        function checkCorrect(textContent) {
+            //console.log("reach");
+            if (questionList[currentQuestion].answer === textContent)
+                ++numScores;
+            qHeader.textContent = "";
+            aHeader.textContent = "";
+            ++currentQuestion;
         }
     }
-
-    checkCorrect(textContent); {
-        if (question.answer == textContent)
-            ++correct;
-        else
-            time -= 10;
-        return;
-    }
-    quizStart();
 }
