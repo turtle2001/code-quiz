2a3e5b8e457280aadc711407fb70dfee1d4c1055
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-03 21:35:45 -0700

    trying to display question

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,44 @@
 var question0 = {
     question: q1,
-    answer: ["q", "w", "e", "r"]
+    answers: ["q", "w", "e", "r"],
+    answer: answers[0]
 }
 
-var question = [question0];
+var questions = [question0];
 var highscores;
 var numScores;
-var currentQ;
+//var currentQ;
 
-startButton.addEventListener("clcik", function(){
+startButton.addEventListener("clcik", function () {
+    var cuurentQ = 0;
+    var correct = 0;
 
+    quizStart();
 })
+
+function quizStart() {
+    if (currentQuestion < questions.length) {
+        var question = questions[currentQuestion];
+        var qHeader = document.createElement("h2");
+        qHeader.setAttribute("id", "qHeader");
+        qHeader.textContent(questions[cuurentQ]);
+
+        for (i = 0; i < questions.answer.length; i++) {
+            var answer = questions;
+            var aHeader = document.createElement("h2");
+            aHeader.setAttribute("id", "aHeader")
+            aHeader.textContent(questions.answer[i]);
+
+            aHeader.addEventListener("click", checkCorrect(textContent))
+        }
+    }
+
+    checkCorrect(textContent); {
+        if (question.answer == textContent)
+            ++correct;
+        else
+            time -= 10;
+        return;
+    }
+    quizStart();
+}
