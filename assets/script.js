f81a8f1c57a1047b8173e406a21aee36ed3c3cf7
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-10-03 20:23:32 -0700

    initail framework

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var question0 = {
+    question: q1,
+    answer: ["q", "w", "e", "r"]
+}
+
+var question = [question0];
+var highscores;
+var numScores;
+var currentQ;
+
+startButton.addEventListener("clcik", function(){
+
+})
